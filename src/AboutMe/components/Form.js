f6e4534f784360d41b37a6d4daa3bd2215a6c6e4
PlewkaMaciej <82128422+PlewkaMaciej@users.noreply.github.com>
2022-06-22 23:28:41 +0200

    add fields in form

--- src/AboutMe/components/Form.js
+++ src/AboutMe/components/Form.js
@@ -1,21 +1,37 @@
 import '../styles/Form.css';
-function Form({setShowForm}) {
- const changeShow=()=>{
-    setShowForm(false)
- }
-  return (
-    <>
-   <div className="main-container-form">
-    <div className='container-for-collapse-button'>
-    <button onClick={changeShow} className='collapse-button'>Collapse the form</button>
-    </div>
-    <div className='form-and-heading-container'>
-    <h1  className='form-heading'>Please fill in the form</h1>
-    </div>
+function Form({ setShowForm }) {
+    const changeShow = () => {
+        setShowForm(false)
+    }
+    return (
+        <>
+            <div className="main-container-form">
+                <div className='container-for-collapse-button'>
+                    <button onClick={changeShow} className='collapse-button'>Collapse the form</button>
+                </div>
+                <div className='form-and-heading-container'>
+                    <h1 className='form-heading'>Please fill in the form</h1>
+                    <form class="contact-form">
+                        <label className='user-info-label' for="fname">First and last name:
+                        <input type="text" required name="name" />
+                        </label>
+                        <label className='user-info-label' for="email">Email:
+                        <input type="email" required name="email" />
+                        </label>
+                        <label className='user-info-label' for="Phone number">Phone Number:
+                        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" required name="number" />
+                        <small>Format: xxx-xxx-xxx</small>
+                        </label>
+                        <label className='user-info-label' for="Message">Message:
+                        <textarea className="user-message" required rows="4" cols="50" type="text" name="message" />
+                        </label>
+                        <button className='collapse-button'> Send</button>
+                    </form>
+                </div>
 
-   </div>
-   </>
-  );
+            </div>
+        </>
+    );
 }
 
 export default Form;
